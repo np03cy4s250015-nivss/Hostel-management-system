8a5d87e190ce4817f8b68c0ecdc67f38c1dbdce0
Bishav Pradhan <bishavpradhan17@gmail.com>
2026-05-19 15:14:31 +0545

    Fixed bug now must have 6 digit password while registering user as well

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -75,6 +75,10 @@ router.post('/students', isAdmin, async (req, res) => {
             return res.status(400).json({ error: 'Missing required fields' });
         }
 
+        if (password.length < 6) {
+            return res.status(400).json({ error: 'Password must be at least 6 characters long' });
+        }
+
         await connection.beginTransaction();
 
         if (roomId) {
